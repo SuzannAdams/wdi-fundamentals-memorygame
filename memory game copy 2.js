f244dba5ccddd2982c31ea9dd20a4be56f831343
SuzannAdams <58324606+SuzannAdams@users.noreply.github.com>
2019-12-01 16:00:38 -0800

    Add files via upload

---
+++ memory game copy 2.js
@@ -0,0 +1,72 @@
+console.log("up and running!");
+
+console.log("User flipped " + cards[cardId].rank);
+
+console.log(cardImage)
+
+console.log(suit)
+
+const cards = ["queen", "queen", "king", "king"];
+cardsInPlay [1,2,3,4]
+cards = "queen";
+cards = "queen";
+cards = "king";
+cards = "king";
+
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage:"images/queen-of=hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+}
+];
+cardOne.push('cardOne'); 
+ var cardOne[1]= "queen";
+ var cardTwo[2] = "queen";
+ var cardThree[3] = "king";
+ var cardFour[4] = "king";
+
+function cardsInPlay(){
+  console.log("User flipped king");
+ If (cardThree) {
+  return "king";
+ } else if(cardFour) {
+  return "king";
+ }
+};
+<script>
+  function showAlert(cardThree===cardFour) {
+  alert ("You found a match!");
+}
+</script>
+console.log("User flipped queen");
+If (cardOne) {
+  return "queen";
+} else if (cardTwo) {
+  return "queen";
+}
+};
+<script>
+  function showAlert(cardOne===carTwo) {
+  alert ("You found a match!");
+}
+</script>
+
+for (var i = 0; i < cards.length; i++) createBoard
+cardElement.setAttribute('attributeName', 'attributeValue');
+console.log(cards [cardID].cardImage)
